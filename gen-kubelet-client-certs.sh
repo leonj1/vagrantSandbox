9022fbf96d5bcd7b25802ee1ce9b22dde8fb66b5
Jose Leon <leonj1@gmail.com>
2018-09-15 10:35:45 -0400

    Adding certs POC

---
+++ gen-kubelet-client-certs.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+declare -A ip_addresses=( ["worker-0"]="192.168.33.101" ["worker-1"]="192.168.33.102" ["worker-2"]="192.168.33.103" )
+
+for instance in worker-0 worker-1 worker-2; do
+cat > ${instance}-csr.json <<EOF
+{
+  "CN": "system:node:${instance}",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "US",
+      "L": "Portland",
+      "O": "system:nodes",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "Oregon"
+    }
+  ]
+}
+EOF
+
+EXTERNAL_IP=${ip_addresses[$instance]}
+
+INTERNAL_IP=${ip_addresses[$instance]}
+
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -hostname=${instance},${EXTERNAL_IP},${INTERNAL_IP} \
+  -profile=kubernetes \
+  ${instance}-csr.json | cfssljson -bare ${instance}
+done
+
